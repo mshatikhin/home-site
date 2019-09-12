c4716144cc33bb47318a933bd917aef2bf95a96f
Mikhail Shatikhin <shatikhin@skbkontur.ru>
2019-09-12 15:47:37 +0500

    add react-images to portfolio page

--- src/pages/Portfolio/PhotosetItems/PhotosetItems.tsx
+++ src/pages/Portfolio/PhotosetItems/PhotosetItems.tsx
@@ -2,25 +2,52 @@
 import styles from "./PhotosetItems.module.css";
 import { Loader } from "../../../components/Loader";
 import { Photo } from "../types";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 interface Props {
   photos: Photo[];
 }
 
-export const PhotosetItems: React.FC<Props> = ({ photos }) => {
-  const urls = photos.map(
-    photo => `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`,
-  );
+export const PhotosetItems: React.FC<Props> = React.memo(({ photos }) => {
+  const [currentIndex, setCurrentImage] = React.useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = React.useState(false);
 
-  if (urls.length === 0) {
+  const images = photos.map(photo => ({
+    source: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`,
+  }));
+
+  if (images.length === 0) {
     return <Loader />;
   }
 
   return (
     <div className={styles.wrapper}>
-      {urls.map((photoUrl, index) => (
-        <img key={index} className={styles.image} src={photoUrl} alt={"Изображение " + index} />
+      {images.map((image, index) => (
+        <img
+          key={index}
+          className={styles.image}
+          src={image.source}
+          alt={"Изображение " + index}
+          onClick={() => handleOpenModal(index)}
+        />
       ))}
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={handleCloseModal}>
+            <Carousel currentIndex={currentIndex} views={images} />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   );
-};
+
+  function handleOpenModal(index: number) {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }
+
+  function handleCloseModal() {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  }
+});
